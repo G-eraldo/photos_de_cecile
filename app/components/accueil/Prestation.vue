c8d3a89b04ae3e2ca2e198aad77b484b2a98d467
geraldo <contact@lafabriqueducode.fr>
2026-08-26 19:30:35 +0200

    style: reduce top margin of Prestation card for better visual alignment

--- app/components/accueil/Prestation.vue
+++ app/components/accueil/Prestation.vue
@@ -55,7 +55,7 @@ const validateAndSubmit = async (e) => {
 </script>
 
 <template>
-  <Card class="max-w-2xl mx-auto p-4 md:p-6 mt-32">
+  <Card class="max-w-2xl mx-auto p-4 md:p-6 mt-12">
     <CardTitle class="text-xl md:text-2xl font-bold mb-4 text-[#613213]">
       Contactez-moi
     </CardTitle>
