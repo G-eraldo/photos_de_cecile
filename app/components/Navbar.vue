e832548b0d2a9f1bd4ea7504a72311e6c4485a7f
G-eraldo <gerald-f@hotmail.fr>
2026-09-15 10:22:24 +0200

    Update Navbar.vue

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -9,7 +9,6 @@ const menuItems = [
   { name: 'Portfolio', href: '/portfolio' },
   { name: 'Tirage photo', href: '/tirage-photo' },
   { name: 'Offrir', href: '/offrir' },
-  { name: 'Réserver', href: '/reservation' },
   { name: 'Contact', href: '/contact' },
 ]
 
